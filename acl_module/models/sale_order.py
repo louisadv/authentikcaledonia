e49f41fb5fd82973da06712f9d3a2f9bd2cf41ca
advences-acl <101096174+advences-acl@users.noreply.github.com>
2022-03-08 16:24:04 +0300

    test preprod

--- acl_module/models/sale_order.py
+++ acl_module/models/sale_order.py
@@ -19,12 +19,15 @@ class SaleOrder(models.Model):
         def compute_taxes(order_line):
             price = order_line.price_unit * (1 - (order_line.discount or 0.0) / 100.0)
             order = order_line.order_id
-            return order_line.tax_id._origin.compute_all(price * order_line.margin_percent, order.currency_id, order_line.product_uom_qty, product=order_line.product_id, partner=order.partner_shipping_id)
+            return order_line.tax_id._origin.compute_all(price, order.currency_id, order_line.product_uom_qty,
+                                                         product=order_line.product_id,
+                                                         partner=order.partner_shipping_id)
 
         account_move = self.env['account.move']
         for order in self:
-            tax_lines_data = account_move._prepare_tax_lines_data_for_totals_from_object(order.order_line, compute_taxes)
+            tax_lines_data = account_move._prepare_tax_lines_data_for_totals_from_object(order.order_line, compute_taxes, order.margin_percent)
             tax_totals = account_move._get_tax_totals(order.partner_id, tax_lines_data, order.amount_total, order.amount_untaxed, order.currency_id)
+
             order.tax_totals_json = json.dumps(tax_totals)
 
 
