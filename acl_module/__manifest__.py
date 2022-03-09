4737c3fce571d1dd206eec867bd85e0c331f9bfc
advences-acl <101096174+advences-acl@users.noreply.github.com>
2022-03-09 10:34:20 +0300

    Update __manifest__.py

--- acl_module/__manifest__.py
+++ acl_module/__manifest__.py
@@ -3,11 +3,10 @@
     'name': "adv_booking",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Module pour calculer le TGC à partir de la marge""",
 
     'description': """
-        Long description of module's purpose
+        Module pour calculer le TGC à partir de la marge
     """,
 
     'author': "Advences",
